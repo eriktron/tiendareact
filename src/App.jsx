4364abe446fb0c905ed6a78c5821e5062c0d04c6
etron <contreras.eee@gmail.com>
2024-05-15 09:15:22 -0400

    preparando proyecto tienda react 1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-lime-200">
+          Hola Bebe
+      </div>
+    </>
+  )
+}
+
+export default App
