d50578f12db92f903690999bb17fe633f9330afa
etron <contreras.eee@gmail.com>
2024-05-17 12:01:49 -0400

    concluyendo la api mostrando en home y se actualizo todas las dependencias con la de la clase ademas en el main.jsx se tiene una direccion especifica del app.jsx no permite simplemente la direccion a la carpeta

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -7,19 +7,25 @@ function Home() {
   const [items, setItems] = useState(null)
 
   useEffect( ()=>{
-    fetch('https://api.escuelajs.co/api/v1/products')
+    // fetch('https://api.escuelajs.co/api/v1/products')
+    fetch('https://fakestoreapi.com/products')
+    // fetch('https://dummyjson.com/products')
       .then(response => response.json())
       .then(data => setItems(data))
+      // .then(response => console.log(response.json()))
   }, [])
 
   return (    
       <Layaout>
         Home
-        {
-          items?.map( () => (<Card />)            
-          )
-        }
-       
+        <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+          {
+            items?.map( item => (
+              <Card key={item.id} data={item}/>
+            ))
+          }
+        </div>
+       {/* <Card /> */}
       </Layaout>
   )
 }
