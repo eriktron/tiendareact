e64ac7eba7fde35de30696c0f0b0bf758fd27ab6
etron <contreras.eee@gmail.com>
2024-05-20 10:02:49 -0400

    haciendo que muestre el detalle del producto

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,8 +1,12 @@
+import { useContext } from 'react'
+import { TiendaCartaContext } from '../../Context'
 import './styles.css'
 
 const ProductDetail = () => {
+    const context = useContext(TiendaCartaContext)
     return (
-        <aside className='product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white'>
+        <aside 
+            className={`${context.isProductDetailOpen ? 'flex' : 'hidden' } product-detail flex-col fixed right-0 border border-black rounded-lg bg-white`}>
             <div className='flex justify-between items-center p-6'>
                 <h2 className='font-medium text-xl'>Detalle</h2>
                 <div>x</div>
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,12 +4,19 @@ export const TiendaCartaContext = createContext()
 
 export const TiendaCartaProvider = ( {children} ) => {
     const [count, setCount] = useState(0)
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
     console.log('COUNT:', count)
+
+    const openProductDetail = () => setIsProductDetailOpen(true)
+    const closeProductDetail = () => setIsProductDetailOpen(false)
     
     return(
         <TiendaCartaContext.Provider value={{
             count,
-            setCount
+            setCount,
+            openProductDetail,
+            closeProductDetail,
+            isProductDetailOpen,
         }}>
             {children}
         </TiendaCartaContext.Provider>
